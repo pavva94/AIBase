25e33db3b43dd9d0879c177370c4bb3aa22e8d92
Alessandro Pavesi <pavesialessandro@live.it>
2020-05-09 17:29:23 +0200

    Added CNN with Mnist

---
+++ CNNmnist.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+from SimpleCnn import CNN
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.grid(False)
+    plt.imshow(train_images[i])
+plt.show()
+
+# Add a channels dimension
+train_images = train_images[..., tf.newaxis]
+test_images = test_images[..., tf.newaxis]
+
+# Create an instance of the model
+model = CNN(class_len=len(np.unique(train_labels)), input_shape=(28, 28, 1))
+model.build(input_shape=(1, 28, 28, 1))
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+history = model.fit(train_images, train_labels, epochs=10,
+                    validation_data=(test_images, test_labels))
+
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print(test_acc)
